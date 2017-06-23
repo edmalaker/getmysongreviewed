cdb0737471408b5fc2510392e2b76635e94aeba7
Ed Malaker <edmalaker@gmail.com>
2017-06-23 14:16:28 -0400

    Add files via upload

---
+++ termsContent.php
@@ -0,0 +1,66 @@
+<h1 class="col-xs-12 text-center">Terms of Use</h1>
+<p class="col-xs-12">Revision date: 09/06/2014</p>
+
+<p class="col-xs-12">
+All services provided by getmysongreviewed.com ("getmysongreviewed.com") may be used for lawful purposes only. Transmission, storage or presentation of any information, data or material 
+in violation of any United States federal, state or city law is prohibited. This includes, but is not limited to: copyrighted material, material we judge to be threatening or obscene, or 
+material protected by trade secret and other statute. The subscriber agrees to indemnify and hold harmless getmysongreviewed.com from any claims resulting from the use of service which damages the subscriber or any other party.
+</p>
+<p class="col-xs-12">
+Prohibited is ANYTHING that promotes any illegal activity or present content that may be damaging to getmysongreviewed.com' servers, or any other server on the Internet. Links to such 
+materials are also prohibited.
+<br><br>
+Examples of unacceptable content or links:
+<br><br>
+pirated software<br> 
+hacker programs or archives<br> 
+Warez sites<br><br>
+
+<p class="col-xs-12">
+NOTICE: IF YOUR ACCOUNT IS FOUND TO CONTAIN ILLEGAL ACTIVITY, ILLEGAL MP3 FILES, PIRATED SOFTWARE, HACKER PROGRAMS, WAREZ PROGRAMS, OR ANY OTHER ILLEGAL FILES, YOUR ACCOUNT WILL BE 
+TERMINATED IMMEDIATELY,  ADDITIONALLY, getmysongreviewed.com WILL NOTIFY THE PROPER AUTHORITIES OF YOUR ACTIONS.
+</p>
+
+<p class="col-xs-12">
+IRC
+We currently do not allow IRC or IRC bots to be operated on our servers.
+<br><br>
+Server Abuse
+Any attempt to undermine or cause harm to a server or customer of getmysongreviewed.com is strictly prohibited. As our customer you are responsible for all your accounts. Should you 
+violate the Terms of Services outlined within, your account will be cancelled.
+</p>
+
+<p class="col-xs-12">
+Refusal of Service:<br />
+We reserve the right to refuse, cancel or suspend service, at our sole discretion.
+</p>
+
+<p class="col-xs-12">
+Limitation of Liability:<br />
+getmysongreviewed.com shall not be responsible for any claimed damages, including incidental and consequential damages, which may arise from getmysongreviewed.com ' servers going off-line 
+or being unavailable for any reason whatsoever. Furthermore, getmysongreviewed.com shall not be responsible for any claimed damages, including incidental or consequential damages, 
+resulting from the corruption or deletion of any web site from one of getmysongreviewed.com ' servers. All damages shall be limited to the immediate termination of service.
+</p>
+<p class="col-xs-12">
+Violations:<br />
+Violations of these Acceptable Use Policies should be referred to admin@getmysongreviewed.com. All complaints will be investigated promptly. Failure to follow any term or condition 
+will be grounds for immediate account deactivation.
+</p>
+<p class="col-xs-12">
+Disclaimer:<br />
+getmysongreviewed.com cannot be held liable for system down time, crashes or data loss. We cannot be held liable for any predicated estimate of profits which a client would have gained 
+if their site was functioning. Moreover, getmysongreviewed.com holds no responsibility for the use of our clients' accounts. Failure to comply with any terms or 
+conditions will result in the automatic deactivation of the account in question. We reserve the right to remove any account, without advance notice for any reason without restitution, 
+as getmysongreviewed.com sees fit.
+</p>
+
+<p class="col-xs-12">
+Account Activation:<br />
+By activating your account with getmysongreviewed.com, you agree to the above policies and disclaimer. 
+<br><br>
+NOTICE: If you sign up for an account and fail to comply with these terms we will advise you by e-mail prior to taking any action to provide you with an opportunity to correct the problem.
+<br><br>
+getmysongreviewed.com reserves the right to amend any or all of the above policies, guidelines and disclaimer without notification. 
+</p>
+
+<h3><a href = "http://getmysongreviewed.com/" title = "getmysongreviewed.com" target_blank>Home</a></h3>
