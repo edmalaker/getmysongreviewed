84be99059b0213c35c96216b6aad1f775061b4cb
Ed Malaker <edmalaker@gmail.com>
2017-06-23 14:12:35 -0400

    Add files via upload

---
+++ about_content2.php
@@ -0,0 +1,30 @@
+
+<h1 class="col-xs-12 text-center">About</h1>
+
+		<p class="col-xs-12">
+		Get My Song Reviewed (getmysongreviewed.com) is a website created for songwriters, musicians, and the audio recording enthusiast. It allows these artists to get reviews of their songs 
+		by doing reviews of other songs from other artists. There is no charge, the service is completely free. An artist will get a review back for every review the artist completes.
+		</p>
+		
+		<p class="col-xs-12">
+		The reviews are anonymous. The song being reviewed and the reviewer are unknown to the other party. Get My Song Reviewed (getmysongreviewed.com) feels that this is the best way to get the most 
+		honest reviews. 
+		</p>
+		
+		<p class="col-xs-12">
+		The reviews are simple. Multiple choice questions that an artist can complete as the song plays. There are no typed sections in the reviews. An artist can do up to 20 reviews 
+		per song that they upload, after which, the artist can choose a new song (there is no reason it couldn't be the same song). An artist can upload only one song at a time.
+		</p>
+		
+		<p class="col-xs-12">
+		There are many sites available to an artist for networking, getting fans, posting their gigs etc. but not so many for getting honest feedback of their music.  Where honest 
+		feedback does exist it is often expensive, or requires long typed out passages that can make doing reviews seem more like a job than relaxing and listening to music.
+		</p>
+		
+		
+		<p class="col-xs-12">
+		Get My Song Reviewed (getmysongreviewed.com) hopes that this website will be useful, and be a tool that is simple to use and helps an artist create better songs.
+		</p>
+		
+		
+	<h3><a href = "http://getmysongreviewed.com/" title = "getmysongreviewed.com" target_blank>Home</a></h3>
---
+++ ender.php
@@ -0,0 +1,58 @@
+<h3 class="col-xs-12 text-center">Get My Song Reviewed hopes that it can help you create better music and write better songs. <br />Thank you for visiting!</h3>
+
+	 <span class="col-xs-12 text-center"> 
+	 <a href="index.php">Home</a>
+	 </span>
+	 
+	 <div
+  class="fb-like"
+  data-share="true"
+  data-width="450"
+  data-show-faces="true">
+</div>
+
+<div class="col-xs-12 text-center">
+
+<a href="http://32hertz.com/5-record-labels-accepting-music-right-now/" target="_blank"><img src="content/5linksimg.jpg" alt="5 FREE links to record labels accepting demos" height="84" width="150"></a>
+</div>
+
+<div class="col-xs-12 text-center">
+
+<a href="https://twitter.com/GetMySongReview" class="twitter-follow-button" data-show-count="false" data-show-screen-name="false">Follow @GetMySongReview</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+
+<a href="https://twitter.com/share" class="twitter-share-button" data-via="GetMySongReview" data-count="none">Tweet</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>	
+</div>
+	 
+	
+	
+
+
+
+<span class="col-xs-12 text-center"> 
+	 created by Ed Malaker and <a href="http://32hertz.com/" target="_blank">32hertz.com</a> &copy; copyright 2014-2017 Reproduction Strictly Prohibited. All Rights Reserved
+	</span>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-54598207-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+</div><!-- end container -->
+
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+	
+	
+  </body>
+</html>
